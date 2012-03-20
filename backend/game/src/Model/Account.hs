5d8ad07f0016445be02c5a4da80c48722b927be9
edgar <edgar@localhost>
2012-03-20 17:48:21 +0100

    lalala

---
+++ backend/game/src/Model/Account.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+module Model.Account where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+$(genRecord "Account" [             
+                    ("id", ''Id),
+                    ("firstname", ''String),
+                    ("lastname", ''String),
+                    ("picture_small", ''String),
+                    ("picture_medium", ''String),
+                    ("picture_large", ''String),
+                    ("level", ''Integer),
+                    ("skill_accelleration", ''Integer),
+                    ("skill_braking", ''Integer),
+                    ("skill_control", ''Integer),
+                    ("skill_reactions", ''Integer),
+                    ("skill_intelligence", ''Integer),
+                    ("money", ''Integer),
+                    ("respect", ''Integer),
+                    ("diamonds", ''Integer),
+                    ("energy", ''Integer),
+                    ("max_energy", ''Integer),
+                    ("energy_recovery", ''Integer),
+                    ("energy_update", ''Integer),
+                    ("busy_until", ''Integer)
+    ]
+    )
+$(genDatabase "Account")
+$(genInstance "Account" [             
+                    ("id", ''Id),
+                    ("firstname", ''String),
+                    ("lastname", ''String),
+                    ("picture_small", ''String),
+                    ("picture_medium", ''String),
+                    ("picture_large", ''String),
+                    ("level", ''Integer),
+                    ("skill_accelleration", ''Integer),
+                    ("skill_braking", ''Integer),
+                    ("skill_control", ''Integer),
+                    ("skill_reactions", ''Integer),
+                    ("skill_intelligence", ''Integer),
+                    ("money", ''Integer),
+                    ("respect", ''Integer),
+                    ("diamonds", ''Integer),
+                    ("energy", ''Integer),
+                    ("max_energy", ''Integer),
+                    ("energy_recovery", ''Integer),
+                    ("energy_update", ''Integer),
+                    ("busy_until", ''Integer)
+    ])
+
+
---
+++ backend/game/src/Model/General.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE RankNTypes, MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances, FlexibleContexts #-}
+module Model.General where 
+
+
+import           Data.SqlTransaction
+import qualified Data.HashMap.Strict as S  
+import qualified Data.Map as M 
+import qualified Database.HDBC as H
+import qualified Data.Aeson as A
+import           Data.Maybe 
+import           Data.Convertible
+import           Control.Monad
+
+class H.IConnection c => Database c a where 
+    save :: a -> SqlTransaction c Integer
+    load :: Integer -> SqlTransaction c (Maybe a) 
+
+
+class Mapable a where 
+    fromMap :: M.Map String SqlValue -> Maybe a
+    toMap :: a -> M.Map String SqlValue  
+    fromHashMap :: S.HashMap String SqlValue -> Maybe a 
+    toHashMap :: a -> S.HashMap String SqlValue 
+
+type Id = Maybe Integer 
+
+
+mlookup :: Convertible SqlValue a => String -> M.Map String SqlValue -> Maybe a 
+mlookup = (fmap convert .). M.lookup 
+
+sempty :: M.Map String SqlValue 
+sempty = M.empty 
+
+sinsert = M.insert
+
+htsql :: Convertible a H.SqlValue => a -> H.SqlValue 
+htsql = convert
+
+nempty :: S.HashMap String SqlValue 
+nempty = S.empty 
+
+ninsert = S.insert 
+
+nlookup :: Convertible SqlValue a => String -> S.HashMap String SqlValue -> Maybe a
+nlookup = (fmap convert .). S.lookup 
+
+nhead = ((fmap . fmap) join . fmap) (fmap fromHashMap . listToMaybe)
+thsql :: H.SqlValue -> Integer 
+thsql = convert 
+mco = (fmap) thsql 
+upsert = undefined 
