6f1c0cb689eff1a45824100209be738e34a8611f
edgar <edgar@localhost>
2012-03-26 19:39:01 +0200

    cleanup

--- backend/proxy/src/Database/Media.hs
+++
@@ -1,9 +0,0 @@
-module Database.Media where 
-
-import Data.SqlTransaction
-
-mediaRoute :: IConnection c => Int -> SqlTransaction c (String, String)
-mediaRoute x = do 
-    p <- sqlGetOne "select servername from media_task where id=?" [toSql x] 
-    let (x, z) = break (==':') $ fromSql p
-    return (x, tail z)
