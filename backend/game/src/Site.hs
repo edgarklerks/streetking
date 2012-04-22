81021ddd6fb2e0738060a1e31b691e45e1bd843a
edgar <edgar@localhost>
2012-04-22 18:53:22 +0200

    sad

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -140,7 +140,7 @@ marketManufacturer = do
 marketModel :: Application ()
 marketModel = do 
       ((l,o),xs) <-  getPagesWithDTD ("manufacturer_id" +== "manufacturer_id")
-      ns <- runDb (search xs [] l o) :: Application [C.Car]
+      ns <- runDb (search xs [] l o) :: Application [Car.Car]
       writeMapables ns
 
 marketBuy :: Application ()
