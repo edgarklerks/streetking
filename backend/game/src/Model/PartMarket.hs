90aace00f8ba6ca9764f2c5e5a1b9d32ff74a885
edgar <edgar@localhost>
2012-04-22 14:41:59 +0200

    added part market

---
+++ backend/game/src/Model/PartMarket.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
+module Model.PartMarket where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+
+$(genAll "PartMarket" "market_parts" [
+    ("id", ''Id),
+    ("manufacturer_id", ''Integer),
+    ("part_type_id", ''Integer),
+    ("weight", ''Integer),
+    ("parameter", ''Integer),
+    ("car_id", ''Id),
+    ("d3d_model_id", ''Integer),
+    ("level", ''Integer),
+    ("name", ''String)
+ ])
+
+
