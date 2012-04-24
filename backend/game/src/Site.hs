04909fa6534b8dcc5089d29cabdc2796e1b2a3e1
edgar <edgar@localhost>
2012-04-24 13:55:50 +0200

    repae

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -136,7 +136,7 @@ loadMenu = do
 marketManufacturer :: Application ()
 marketManufacturer = do 
        ((l, o),xs) <- getPagesWithDTD ("id" +== "manufacturer_id") 
-       ms <- runDb (search [] [] l o) :: Application [M.Manufacturer]
+       ms <- runDb (search xs [] l o) :: Application [M.Manufacturer]
        writeMapables ms 
 
 marketModel :: Application ()
