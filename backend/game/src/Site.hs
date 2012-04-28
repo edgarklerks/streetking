f9743758e6021c4a69e9848adfcbfc035edfdfb1
edgar <edgar@localhost>
2012-04-28 22:09:55 +0200

    added not showing deleted parts

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -277,10 +277,10 @@ marketSell = do
                     
                     let mny = A.money a - abs fee
                     when (mny < 0) $ rollback "Not enough money, fuckface"
-                    when (Mi.price d < 0) $ rollback "Price should be positive" 
+                    when (MI.price d < 0) $ rollback "Price should be positive" 
                     -- save part to market  
 
-                    save (d {MI.account_id = uid, price = abs (Mi.price d)  })
+                    save (d {MI.account_id = uid, price = abs (MI.price d)  })
                     
 
                     -- save part_instance as loon item 
