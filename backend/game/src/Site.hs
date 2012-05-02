ce6467b4bfe9fbb5f8295348fe72d8df0be24e12
edgar <edgar@localhost>
2012-05-02 21:36:51 +0200

    adsad

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -491,8 +491,7 @@ carParts = do
     uid <- getUserId
     ((l,o),xs) <- getPagesWithDTD (
         "car_instance_id" +== "car_instance_id" +&& 
-        "part_instance_id" +== "part_instance_id" +&&
-        "account_id" +==| (toSql uid))
+        "part_instance_id" +== "part_instance_id" 
     ns <- runDb $ search xs [] l o :: Application [CIP.CarInstanceParts]
     writeMapables ns
 
