f4ea9f55c366ac1c87062652249d226dd8b62236
edgar <edgar@localhost>
2012-05-03 19:42:02 +0200

    added check on car market for level

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -774,9 +774,12 @@ addPart = do
             Just x -> do 
                 p <- search ["part_instance_id" |== toSql (MI.part_instance_id d)] [] 1 0 :: SqlTransaction Connection [GPT.GaragePart]
                 case p of 
-                    [] -> rollback "impossubru happended"
+                    [] -> rollback "impossubru happended" *>
+                            liftIO $ print p
                     [p] -> do 
                         xs <- search ["part_type_id" |== toSql (GPT.part_type_id p) .&& "car_instance_id" |== toSql (PI.car_instance_id x)] [] 1 0 :: SqlTransaction Connection [CIP.CarInstanceParts]
+                        liftIO $ print p
+                        liftIO $ print xs 
                         case xs of 
                             [] -> do  
 
