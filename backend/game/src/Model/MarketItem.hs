51f7604177a69a149eafe3339e45aa6eb4b1eae4
edgar <edgar@localhost>
2012-05-03 13:21:55 +0200

    added car sell

--- backend/game/src/Model/MarketItem.hs
+++ backend/game/src/Model/MarketItem.hs
@@ -12,6 +12,7 @@ import           Control.Applicative
 import qualified Data.Map as M
 import           Model.TH
 import           Prelude hiding (id)
+
 type MInteger = Maybe Integer 
 $(genAll "MarketItem" "market_item"
     [
