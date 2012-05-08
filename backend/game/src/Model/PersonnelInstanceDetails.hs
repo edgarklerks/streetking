bb61e4e447ac1ed81966bd5c70a8131b5ccf2bc7
edgar <edgar@localhost>
2012-05-08 11:31:02 +0200

    file added

---
+++ backend/game/src/Model/PersonnelInstanceDetails.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
+module Model.PersonnelInstanceDetails where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Data.InRules
+import           Control.Monad
+
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+
+type MInteger = Maybe Integer 
+$(genAllId "PersonnelInstanceDetails" "personnel_instance_details" "personnel_instance_id" [
+    ("personnel_instance_id", ''Id),
+    ("personnel_id", ''Id),
+    ("name", ''String),
+    ("country_name", ''String),
+    ("country_shortname", ''String),
+    ("gender", ''Bool),
+    ("picture", ''String),
+    ("salary", ''Integer),
+    ("skill_repair", ''Integer),
+    ("skill_engineering", ''Integer),
+    ("busy_until", ''Integer),
+    ("paid_until", ''Integer)
+    ])
