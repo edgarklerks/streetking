8c636e33f0447f0d87ece3ca93fa9f830f25de85
justin <justin@localhost>
2012-05-10 16:46:02 +0200

    foo

---
+++ backend/game/src/Data/Hstore.hs
@@ -0,0 +1,75 @@
+module Data.Hstore (
+        parseHStore,
+        HStore,
+        ppHStore 
+    ) where 
+
+import qualified Data.HashMap.Strict as S
+import Text.Parsec.String
+import Text.Parsec.Combinator
+import Text.Parsec.Char
+import Text.Parsec.Prim hiding (many, (<|>))
+import Control.Applicative
+import Database.HDBC
+import qualified Data.Foldable as F
+import Data.List 
+
+type HStore = S.HashMap String String 
+
+ppHStore :: HStore -> String 
+ppHStore xs = intercalate "," $ 
+              do (k,v) <- S.toList xs 
+                 return $ '"' : (escapeString k ++ "\"=>" ++ ('"' : escapeString v ++ ['"'])) 
+ 
+escapeString =  concatMap step 
+    where 
+        step '"' = "\\\""
+        step x = [x]
+
+parseHStore :: SqlValue -> HStore 
+parseHStore xs =  case parse parseEntries "" (fromSql xs :: String) of 
+                            Left e -> error (show e)
+                            Right xs -> S.fromList xs 
+
+parseEntries :: Parser [(String,String)]
+parseEntries = parseEntry `sepBy` (char ',' <* spaces)
+
+parseEntry :: Parser (String, String)
+parseEntry = (,) <$> (parseKey <* string "=>") <*> parseValue 
+
+
+parseKey :: Parser String 
+parseKey = parseString <|> (many $ oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.")
+
+
+parseValue :: Parser String 
+parseValue = parseString <|> parseInt <|> parseNull 
+
+parseNull :: Parser String 
+parseNull = string "NULL" *> pure ""
+
+parseInt :: Parser String 
+parseInt = many (parseNumber <|> parseSymbol)
+
+parseSymbol :: Parser Char
+parseSymbol = oneOf "-."
+
+parseNumber :: Parser Char 
+parseNumber = oneOf "0123456789"
+
+parseString :: Parser String 
+parseString = char '"' *> many charp <* char '"'
+
+charp :: Parser Char  
+charp = esccharp <|> uncharp
+
+esccharp :: Parser Char
+esccharp =  char '\\'  *> charp 
+
+uncharp :: Parser Char 
+uncharp = noneOf "\"" 
+
+
+
+
+
