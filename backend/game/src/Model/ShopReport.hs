7a7f5ec83d63d2b74ea8d7696fc7ba2bbea4a4a3
edgar <edgar@localhost>
2012-05-15 21:23:42 +0200

    lalal

--- backend/game/src/Model/ShopReport.hs
+++ backend/game/src/Model/ShopReport.hs
@@ -14,6 +14,7 @@ import           Model.TH
 import           Prelude hiding (id)
 
 type MInteger = Maybe Integer
+type MString = Maybe String 
 
 $(genAll "ShopReport" "shopping_reports" 
     [ ("id", ''Id),
@@ -24,6 +25,34 @@ $(genAll "ShopReport" "shopping_reports"
         ("report_descriptor", ''String),
         ("part_instance_id", ''MInteger),
         ("car_instance_id",''MInteger),
-        ("part_id", ''Integer),
-        ("amount", ''Integer)
+        ("car_id", ''MInteger),
+        ("part_id", ''MInteger),
+        ("amount", ''Integer),
+        ("car_manufacturer_name", ''MString),
+        ("car_top_speed", ''MInteger),
+        ("car_acceleration", ''MInteger),
+        ("car_braking", ''MInteger),
+        ("car_nos", ''MInteger),
+        ("car_handling", ''MInteger),
+        ("car_name", ''MString),
+        ("car_level", ''MInteger),
+        ("car_year", ''MInteger),
+        ("car_price", ''MInteger),
+        ("car_weight", ''Minteger),
+        ("car_improvement", ''MInteger),
+        ("car_wear", ''MInteger),
+        ("part_weight", ''MInteger),
+        ("part_level", ''MInteger),
+        ("part_car_model", ''MString),
+        ("part_parameter1", ''MInteger),
+        ("part_parameter2", ''MInteger),
+        ("part_parameter3", ''MInteger),
+        ("part_parameter1_type", ''MString),
+        ("part_parameter2_type", ''MString),
+        ("part_parameter3_type", ''MString),
+        ("part_modifier", ''MString),
+        ("part_unique", ''Bool),
+        ("part_improvement", ''MInteger),
+        ("part_wear", ''MInteger),
+        ("part_manufacturer_name", ''MString)
     ])
