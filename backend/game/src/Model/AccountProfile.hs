beb429222accd87e3508b37e94e11da47e4f3cdc
justin <justin@localhost>
2012-05-19 17:23:24 +0200

    a

--- backend/game/src/Model/AccountProfile.hs
+++ backend/game/src/Model/AccountProfile.hs
@@ -37,10 +37,10 @@ $(genAll "AccountProfile" "account_profile" [
                     ("till", ''Integer),
                     ("city_id", ''Integer),
                     ("city_name", ''Integer),
-                    ("city_data", ''Integer),
+                    ("city_data", ''String),
                     ("continent_id", ''Integer),
                     ("continent_name", ''Integer),
-                    ("continent_data", ''Integer),
+                    ("continent_data", ''String),
                     ("skill_unused", ''Integer)
     ]
     )
