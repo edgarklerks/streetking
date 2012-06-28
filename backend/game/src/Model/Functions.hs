179720184a1cfbc2dc6528d015bc283adfbe2023
justin <justin@localhost>
2012-06-28 16:13:37 +0200

    bar

---
+++ backend/game/src/Model/Functions.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Model.Functions where 
+
+import Model.DBFunctions 
+import Data.SqlTransaction
+import Database.HDBC (toSql, fromSql)
+import Data.Database
+import Data.HashMap.Strict
+
+$(mkFunctions [
+    ("account_update_energy", [''Integer], ''Bool, Scalar),
+    ("personnel_train", [''Integer, ''String, ''String], ''Bool, Scalar),
+    ("personnel_start_task", [''Integer, ''String, ''Integer], ''Bool, Scalar),
+    ("personnel_cancel_task", [''Integer], ''Bool, Scalar),
+    ("garage_actions_account", [''Integer], ''Bool, Scalar),
+    ("garage_car_ready", [''Integer, ''Integer], ''String, Row),
+    ("garage_active_car_ready", [''Integer], ''String, Row),
+    ("garage_set_active_car", [''Integer, ''Integer], ''Bool, Scalar),
+    ("garage_unset_active_car", [''Integer, ''Integer], ''Bool, Scalar)
+    ])
