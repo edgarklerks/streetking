8ced99e57e890d55ab9a361f147e8db57fe56c57
edgar <edgar@localhost>
2012-07-02 20:41:36 +0200

    blamsad

--- backend/proxy/src/Site.hs
+++ backend/proxy/src/Site.hs
@@ -18,8 +18,6 @@ import           Data.Maybe
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text.Encoding as T
-import           Snap.Extension.Heist
-import           Snap.Extension.Timer
 --import           Snap.Extension.Decoder
 import           Snap.Util.FileServe
 import           Snap.Types
@@ -209,7 +207,7 @@ roleUser = do
             [User (Nothing)] -> writeLBS "{\"result\":0}" 
 
 
-    (not.null) <$>  getRoles "user_token" >>= writeLBS .  ("{\"result\":" `BL.append` ) . (`BL.append` "}") . A.encode
+--    (not.null) <$>  getRoles "user_token" >>= writeLBS .  ("{\"result\":" `BL.append` ) . (`BL.append` "}") . A.encode
 
 
 -- cross domain shizzle for unity
