db1f99bd40b78b587d5ead7a59ba7492c63229cd
edgar <edgar@localhost>
2012-07-03 16:57:17 +0000

    added images

---
+++ backend/images/src/Application.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+------------------------------------------------------------------------------
+-- | This module defines our application's state type and an alias for its
+-- handler monad.
+module Application where
+
+------------------------------------------------------------------------------
+import Data.Lens.Template
+import Snap.Snaplet
+import Snap.Snaplet.Heist
+import Snap.Snaplet.Auth
+import Snap.Snaplet.Session
+import ConfigSnaplet
+import SqlTransactionSnaplet
+import ImageSnapLet
+
+------------------------------------------------------------------------------
+data App = App
+    { _heist :: Snaplet (Heist App)
+    , _sess :: Snaplet SessionManager
+    , _auth :: Snaplet (AuthManager App)
+    , _config :: Snaplet ConfigSnaplet 
+    , _sql :: Snaplet SqlTransactionConfig
+    , _img :: Snaplet ImageConfig 
+    }
+
+makeLens ''App
+
+instance HasHeist App where
+    heistLens = subSnaplet heist
+
+instance HasConfig App where 
+    configLens = subSnaplet config 
+
+instance HasSqlTransaction App where 
+    sqlLens = subSnaplet sql 
+
+instance HasImageSnapLet App where 
+    imageLens = subSnaplet img 
+------------------------------------------------------------------------------
+type AppHandler = Handler App App
+
+
