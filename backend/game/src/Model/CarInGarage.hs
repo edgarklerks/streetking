174dd3897bb31c57286922cdd3bf34062a61eb2d
justin <justin@localhost>
2012-07-20 15:05:18 +0200

    racedata

---
+++ backend/game/src/Model/CarInGarage.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
+module Model.CarInGarage where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Aeson as AS
+
+$(genAll "CarInGarage" "car_in_garage"
+    [
+        ("id", ''Id),
+        ("car_id", ''Integer),
+        ("manufacturer_name", ''String),
+        ("manufacturer_picture", ''String),
+        ("weight", ''Integer),
+        ("top_speed", ''Integer),
+        ("acceleration", ''Integer),
+        ("stopping", ''Integer),
+        ("cornering", ''Integer),
+        ("nitrous", ''Integer),
+        ("power", ''Integer),
+        ("traction", ''Integer),
+        ("handling", ''Integer),
+        ("braking", ''Integer),
+        ("aero", ''Integer),
+        ("nos", ''Integer),
+        ("name", ''String),
+        ("parts_price", ''Integer),
+        ("total_price", ''Integer),
+        ("account_id", ''Integer),
+        ("level", ''Integer),
+        ("wear", ''Integer),
+        ("improvement", ''Integer),
+        ("active", ''Bool),
+        ("ready", ''Bool),
+        ("year", ''Integer),
+        ("car_color", ''String)
+    ])
+
+instance AS.ToJSON CarInGarage where
+        toJSON c = AS.toJSON $ HM.fromList $ [ 
+                        ("user_id", AS.toJSON $  id c),
+                        ("name", AS.toJSON $  name c)
+                    ]
+
---
+++ backend/game/src/Model/PartModifier.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
+module Model.PartModifier where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+$(genAll "PartModifier" "part_modifier" [
+    ("id", ''Id),
+    ("name", ''String)
+    ])
