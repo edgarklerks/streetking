7ebd6e380df8aa4ad0223c738bbd63816cfffd52
edgar <edgar@localhost>
2012-07-21 11:07:53 +0000

    added routes

---
+++ backend/game-new/src/NodeSnaplet.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE TemplateHaskell, OverloadedStrings, FlexibleContexts #-}
+module NodeSnaplet where 
+
+import Config.ConfigFileParser 
+import MemServerAsync
+import Proto 
+import Data.Lens.Common 
+import Data.Lens.Template
+import Snap.Core 
+import Snap.Snaplet
+import Control.Monad.Trans
+import Control.Monad 
+import Control.Applicative
+import Control.Concurrent
+import Control.Monad.State
+
+data DHTConfig = DHC {
+        _query :: Proto -> IO Proto  
+    }
+
+class HasDHT b where 
+    dhtLens :: Lens (Snaplet b) (Snaplet DHTConfig) 
+
+
+sendQuery :: (MonadState DHTConfig m, MonadIO m) => Proto -> m Proto 
+sendQuery r = do 
+        p <- gets _query 
+        liftIO $ p r
+
+$(makeLenses [''DHTConfig])
+
+initDHTConfig fp = makeSnaplet "DistributedHashNodeSnaplet" "distributed hashnode" Nothing $ do 
+        xs <- liftIO $ readConfig fp  
+        let (Just (StringC ctr)) = lookupConfig "DHT" xs >>= lookupVar "ctrl"
+        let (Just (StringC upd)) = lookupConfig "DHT" xs >>= lookupVar "data"
+        let (Just (ArrayC ns)) = lookupConfig "DHT" xs >>= lookupVar "nodes"
+        let ccl = clientCommand ctr "tcp://127.0.0.1:99912"
+        let cl = client ctr "tcp://127.0.0.1:91281"
+        let (Just (StringC svn)) = lookupConfig "DHT" xs >>= lookupVar "dump"
+        
+        liftIO $ forkIO $ startNode ctr upd svn 
+
+        liftIO $ forkIO $ forever $  do 
+                forM_  (toStrings ns) $ \i ->  do 
+                        (ccl . advertise) i
+                        clientCommand i "tcp://127.0.0.1:999231" (advertise ctr)
+                threadDelay 100000000        
+
+        return $ DHC cl 
+
+
+
+
+toStrings :: [Config] -> [String]
+toStrings xs = toString <$> xs
+toString (StringC p) = p 
---
+++ templates/events/event_travel_city.tpl
@@ -0,0 +1,21 @@
+<div class="travel-cities-point-box" style="[:city_data]">
+	[:when (me.level > (city_level - 1) & me.city_id != city_id)]{
+		<a href="#Track/list/?city=[:city_id]" alt="" title="" module="TRACK_LIST_BY_CITIES">
+			<div class="travel-cities-point" city="[:city_name]" link="#City/travel/?id=[:city_id]">
+				<div class="travel-cities-point-inner"></div>
+			</div>
+		</a>
+		<div class="travel-cities-name ui-corner-all">[:city_name]</div>
+	}[:when (me.city_id == city_id)]{
+		<div class="travel-cities-pointa">
+			<div class="travel-cities-point-innera"></div>
+		</div>
+		<div class="travel-cities-namea ui-corner-all">[:city_name]<br><span>you are here</span></div>
+	}
+	[:when (me.level < city_level)]{
+		<div class="travel-cities-pointd">
+			<div class="travel-cities-point-innerd"></div>
+		</div>
+		<div class="travel-cities-named ui-corner-all">[:city_name]<br><span>from level [:city_level]</span></div>
+	}
+</div>
