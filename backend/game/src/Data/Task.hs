5e9780d692b2988c8f4ea24f1786fe9cf45d0469
justin <justin@localhost>
2012-08-03 14:28:30 +0200

    a

--- backend/game/src/Data/Task.hs
+++ backend/game/src/Data/Task.hs
@@ -25,7 +25,9 @@ import qualified Model.TaskExtended as TKE
 
 {-
 -- TODO: generic data type for task data
--- wrapped record? Aeson / InRule HashMap? 
+-- wrapped record? Aeson / InRule HashMap?
+-- -> use simple record type with to/from aeson only
+-- -> store as bytestring
 -}
 
 $(genMapableRecord "DataModifyMoney" [
@@ -61,34 +63,83 @@ $(genMapableRecord "DataTrackTime" [
         ("dtt_time", ''Double)
     ])
 
--- helper function, copied from Site.hs. This should really be put in a tools file somewhere.
-aget :: Database Connection a => Constraints -> SqlTransaction Connection () -> SqlTransaction Connection a
-aget cs f = do
-    ss <- search cs [] 1 0
-    unless (not $ null ss) f
-    return $ head ss
-
+type Package = C.ByteString
 
 {-
  - Tasks: pre-set low-cost actions to be fired if specified subject is called for after a specified time
  -}
 
-setTask :: Integer -> String -> C.ByteString -> SqlTransaction Connection Integer 
-setTask t tpe d = do
-        save $ (def :: TK.Task) { TK.type = tpe, TK.time = t, TK.data = d, TK.deleted = False  }
 
-setTaskTrigger :: String -> Integer -> Integer -> SqlTransaction Connection Integer 
-setTaskTrigger tpe sid tid = do
-        save $ (def :: TKT.TaskTrigger) { TKT.task_id = tid, TKT.type = tpe, TKT.subject_id = sid }
+{-
+ - Set tasks 
+ -}
 
-unsetTask :: Integer -> SqlTransaction Connection ()
-unsetTask tid = do
-        x <- aget ["id" |== toSql tid] (failTask "unset: task not found") :: SqlTransaction Connection TK.Task
-        save $ x { TK.deleted = True }
+trackTime :: Integer -> Integer -> Integer -> Double -> SqlTransaction Connection Integer
+trackTime t trk uid tme = do
+        tid <- save $ task t "track_time" $ pack $ (def :: DataTrackTime) { dtt_track_id = trk, dtt_account_id = uid, dtt_time = tme }
+        save $ trigger "track" trk tid
+        save $ trigger "user" uid tid
+        return tid
+
+modifyMoney :: Integer -> Integer -> Integer -> SqlTransaction Connection Integer
+modifyMoney t uid amt = do
+        tid <- save $ task t "modify_money" $ pack $ (def :: DataModifyMoney) { dmm_account_id = uid, dmm_amount = amt }
+        save $ trigger "user" uid tid
+        return tid
+
+
+{-
+ - Fire tasks 
+ -}
+
+-- fire all task triggers of a trigger type 
+runAll :: String -> SqlTransaction Connection ()
+runAll tp = do
+        ss <- claim ["subject_type" |== toSql tp]
+        forM ss process
+        return ()    
+
+-- fire task trigger by trigger type and subject ID
+run :: String -> Integer -> SqlTransaction Connection ()
+run tp sid = do
+        ss <- claim ["subject_type" |== toSql tp, "subject_id" |== toSql sid]
+        forM ss process
+        return ()    
+
+
+{-
+ - Internal
+ -}
+
+-- TODO: InRule support for ByteString.Lazy to allow storage as a record column type 
+
+-- pack data
+pack :: forall a. AS.ToJSON a => a -> Package 
+pack = C.pack . LBC.unpack . AS.encode
+
+-- unpack data
+unpack :: forall a. AS.FromJSON a => Package -> Maybe a
+unpack = AS.decode . LBC.pack . C.unpack
+
+-- make a new task with time and data
+task :: Integer -> String -> Package -> TK.Task
+task t tpe d = (def :: TK.Task) { TK.type = tpe, TK.time = t, TK.data = d, TK.deleted = False }
+
+-- make a new task trigger with subject type and subject ID
+trigger :: String -> Integer -> Integer -> TKT.TaskTrigger 
+trigger tpe sid tid = (def :: TKT.TaskTrigger) { TKT.task_id = tid, TKT.type = tpe, TKT.subject_id = sid }
+
+-- mark a task as deleted
+unset :: Integer -> SqlTransaction Connection ()
+unset tid = do
+        ss <- search ["id" |== toSql tid] [] 1 0
+        when (null ss) $ fali $ "unset: task not found: " ++ (show tid)
+        save $ (head ss) { TK.deleted = True }
         return ()
 
-cleanupTasks :: SqlTransaction Connection ()
-cleanupTasks = do 
+-- cleanup deleted tasks (triggers are deleted cascading)
+cleanup :: SqlTransaction Connection ()
+cleanup = do 
         t <- liftIO $ floor <$> getPOSIXTime
         transaction sqlExecute $ Delete (table "task") ["time" |<= SqlInteger (t + 24*60*60), "deleted" |== SqlBool True]
         return ()
@@ -100,28 +151,18 @@ cleanupTasks = do
 -- 3. proceed as normal
 -- -> multiple concurrent searches cannot return the same task
 
-searchCurrentTasks :: Constraints -> SqlTransaction Connection [TKE.TaskExtended]
-searchCurrentTasks xs = do
-        cleanupTasks
+-- claim current tasks by selection constraints, and fetch them
+claim :: Constraints -> SqlTransaction Connection [TKE.TaskExtended]
+claim xs = do
+        cleanup
         t <- liftIO $ floor <$> getPOSIXTime
         search (("time" |<= SqlInteger t) : xs) [] 10000 0 
 
-runTasksAll :: String -> SqlTransaction Connection ()
-runTasksAll tp = do
-        ss <- searchCurrentTasks ["subject_type" |== toSql tp]
-        forM ss processTask
-        return ()    
+-- process a task
+process :: TKE.TaskExtended -> SqlTransaction Connection ()
+process s = do
 
-runTasks :: String -> Integer -> SqlTransaction Connection ()
-runTasks tp sid = do
-        ss <- searchCurrentTasks ["subject_type" |== toSql tp, "subject_id" |== toSql sid]
-        forM ss processTask
-        return ()    
-
-processTask :: TKE.TaskExtended -> SqlTransaction Connection ()
-processTask s = do
-
-        unsetTask $ fromJust $ TKE.id s 
+        unset $ fromJust $ TKE.id s 
         
         case TKE.type s of
 
@@ -135,31 +176,19 @@ processTask s = do
 
                 "give_part" -> undefined 
 
-                "set_top_time" -> do
+                "track_time" -> do
 
-                        let d :: DataTrackTime = fromJust $ AS.decode $ LBC.pack $ C.unpack $ TKE.data s
+                        let d :: DataTrackTime = fromJust $ unpack $ TKE.data s
                         save $ (def :: TTM.TrackTime) { TTM.account_id = dtt_account_id d, TTM.track_id = dtt_track_id d, TTM.time = dtt_time d  }
                         return ()
 
-                foo -> failTask $ "unknown task type: " ++ foo
+                foo -> fali $ "unknown task type: " ++ foo
 
 -- failing tasks should not break transactions
--- TODO: store error report or sth
-failTask :: String -> SqlTransaction Connection ()
-failTask e = return ()
+-- TODO: store error report in db or sth
+
+-- fail a task
+fali :: String -> SqlTransaction Connection ()
+fali e = Data.SqlTransaction.rollback e -- return ()
 
 
--- TODO: pack . unpack: InRule support for lazy bytestrings
-setTrackTime :: Integer -> Integer -> Integer -> Double -> SqlTransaction Connection Integer
-setTrackTime t trk uid tme = do
-        tid <- setTask t "track_time" $ C.pack $ LBC.unpack $ AS.encode $ (def :: DataTrackTime) { dtt_track_id = trk, dtt_account_id = uid, dtt_time = tme }
-        setTaskTrigger "track" trk tid
-        setTaskTrigger "user" uid tid
-        return tid
-
-setModifyMoney :: Integer -> Integer -> Integer -> SqlTransaction Connection Integer
-setModifyMoney t uid amt = do
-        tid <- setTask t "modify_money" $ C.pack $ LBC.unpack $ AS.encode $ (def :: DataModifyMoney) { dmm_account_id = uid, dmm_amount = amt }
-        setTaskTrigger "user" uid tid
-        return tid
- 
