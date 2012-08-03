1ff29544879b165b8a45fb4207cb74ce1ca4b843
justin <justin@localhost>
2012-08-03 19:49:47 +0200

    a

--- backend/game/src/Data/Task.hs
+++ backend/game/src/Data/Task.hs
@@ -9,6 +9,7 @@ import           Data.SqlTransaction
 import           Data.Database
 import           Database.HDBC 
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString.Lazy.Char8 as LBC
 import           Data.Default
 import qualified Data.Aeson as AS
@@ -17,7 +18,7 @@ import           Model.TH
 import           Data.Time.Clock.POSIX
 import           Control.Monad.Trans
 import           Model.General 
-import           Data.Unique.Id
+import           Data.HashMap.Strict as HM
 
 import qualified Model.TrackTime as TTM
 import qualified Model.Task as TK
@@ -31,6 +32,7 @@ import qualified Model.TaskExtended as TKE
 -- -> store as bytestring
 -}
 
+{-
 $(genMapableRecord "DataModifyMoney" [
         ("dmm_account_id", ''Integer),
         ("dmm_amount", ''Integer)
@@ -63,8 +65,55 @@ $(genMapableRecord "DataTrackTime" [
         ("dtt_track_id", ''Integer),
         ("dtt_time", ''Double)
     ])
+-}
 
-type Package = C.ByteString
+
+{-
+ - Data
+ -}
+
+type Key = LB.ByteString
+type Data = HM.HashMap Key AS.Value
+type Pack = C.ByteString -- TODO: InRule support for ByteString.Lazy to allow Pack to be of this type 
+type Action = String -- TODO: enumerate
+
+-- pack data
+pack :: forall a. AS.ToJSON a => a -> Pack 
+pack = C.pack . LBC.unpack . AS.encode
+
+-- unpack data
+unpack :: forall a. AS.FromJSON a => Pack -> Maybe a
+unpack = AS.decode . LBC.pack . C.unpack
+
+-- force unpack data
+unpack' :: forall a. AS.FromJSON a => Pack -> a
+unpack' = fromJust . AS.decode . LBC.pack . C.unpack
+
+-- empty data
+new :: Data
+new = HM.empty 
+
+-- set data field
+set :: forall a. AS.ToJSON a => Key -> a -> Data -> Data
+set k v d = HM.insert k (AS.toJSON v) d
+
+(.=) :: forall a. AS.ToJSON a => Key -> a -> Data -> Data
+(.=) = set 
+infixl 4 .=
+
+-- get data field of specified type
+get :: forall a. AS.FromJSON a => Key -> Data -> Maybe a
+get k d = case fmap AS.fromJSON $ HM.lookup k d of
+        Just (AS.Success v) -> Just v
+        _ -> Nothing
+
+-- force get data field
+get' :: forall a. AS.FromJSON a => Key -> Data -> a
+get' k d = fromJust $ get k d
+
+-- get data field with default
+getd :: forall a. AS.FromJSON a => Key -> Data -> a -> a
+getd k d f = maybe f fromJust $ get k d
 
 
 {-
@@ -73,17 +122,22 @@ type Package = C.ByteString
 
 trackTime :: Integer -> Integer -> Integer -> Double -> SqlTransaction Connection Integer
 trackTime t trk uid tme = do
-        tid <- save $ task t "track_time" $ pack $ (def :: DataTrackTime) { dtt_track_id = trk, dtt_account_id = uid, dtt_time = tme }
-        save $ trigger "track" trk tid
-        save $ trigger "user" uid tid
+        tid <- task t $ "track_id" .= trk $
+                        "account_id" .= uid $
+                        "time" .= tme $
+                        "action" .= ("track_time" :: Action) $
+                        new
+        trigger "track" trk tid
+        trigger "user" uid tid
         return tid
 
+{-
 modifyMoney :: Integer -> Integer -> Integer -> SqlTransaction Connection Integer
 modifyMoney t uid amt = do
         tid <- save $ task t "modify_money" $ pack $ (def :: DataModifyMoney) { dmm_account_id = uid, dmm_amount = amt }
         save $ trigger "user" uid tid
         return tid
-
+-}
 
 {-
  - Fire tasks 
@@ -91,15 +145,16 @@ modifyMoney t uid amt = do
 
 -- fire all task triggers of a trigger type 
 runAll :: String -> SqlTransaction Connection ()
-runAll tp = do
-        ss <- claim ["subject_type" |== toSql tp]
-        forM ss process
-        return ()    
+runAll tp = runWith ["subject_type" |== toSql tp]
 
 -- fire task trigger by trigger type and subject ID
 run :: String -> Integer -> SqlTransaction Connection ()
-run tp sid = do
-        ss <- claim ["subject_type" |== toSql tp, "subject_id" |== toSql sid]
+run tp sid = runWith ["subject_type" |== toSql tp, "subject_id" |== toSql sid]
+
+-- fire tasks by a selection of triggers
+runWith :: Constraints -> SqlTransaction Connection ()
+runWith cs = do 
+        ss <- claim cs
         forM ss process
         return ()    
 
@@ -108,80 +163,58 @@ run tp sid = do
  - Internal
  -}
 
--- TODO: InRule support for ByteString.Lazy to allow storage as a record column type 
-
--- pack data
-pack :: forall a. AS.ToJSON a => a -> Package 
-pack = C.pack . LBC.unpack . AS.encode
-
--- unpack data
-unpack :: forall a. AS.FromJSON a => Package -> Maybe a
-unpack = AS.decode . LBC.pack . C.unpack
-
 -- make a new task with time and data
-task :: Integer -> String -> Package -> TK.Task
-task t tpe d = (def :: TK.Task) { TK.type = tpe, TK.time = t, TK.data = d, TK.deleted = False, TK.claim = 0 }
+task :: Integer -> Data -> SqlTransaction Connection Integer 
+task t a = save $ (def :: TK.Task) { TK.time = t, TK.data = pack a, TK.deleted = False, TK.type = "track_time" }
 
 -- make a new task trigger with subject type, subject ID and task ID
-trigger :: String -> Integer -> Integer -> TKT.TaskTrigger 
-trigger tpe sid tid = (def :: TKT.TaskTrigger) { TKT.task_id = tid, TKT.type = tpe, TKT.subject_id = sid }
-
--- mark a task as deleted
-unset :: Integer -> SqlTransaction Connection ()
-unset tid = do
-        ss <- search ["id" |== toSql tid] [] 1 0
-        when (null ss) $ fali $ "unset: task not found: " ++ (show tid)
-        save $ (head ss) { TK.deleted = True }
-        return ()
-
--- cleanup deleted tasks (triggers are deleted cascading)
-cleanup :: SqlTransaction Connection ()
-cleanup = do 
-        t <- liftIO $ floor <$> getPOSIXTime
-        transaction sqlExecute $ Delete (table "task") ["time" |<= SqlInteger (t - 24*60*60), "deleted" |== SqlBool True]
-        return ()
-
--- TODO: allow concurrent access: claim tasks for retrieval
--- 0. generate unique claim key 
--- 1. update selected tasks: set key in claim field
--- 2. select tasks with claim key
--- 3. proceed as normal
--- -> multiple concurrent searches cannot return the same task
+trigger :: String -> Integer -> Integer -> SqlTransaction Connection Integer 
+trigger tpe sid tid = save $ (def :: TKT.TaskTrigger) { TKT.task_id = tid, TKT.type = tpe, TKT.subject_id = sid }
 
 -- claim current tasks by selection constraints, and fetch them
-claim :: Constraints -> SqlTransaction Connection [TKE.TaskExtended]
-claim xs = do
-        cleanup
-        i <- liftIO $ fmap (hashedId . idFromSupply) $ initIdSupply 't' -- TODO: init not here, probably doesn't work 
+-- TODO: ensure concurrent processes do not claim the same tasks
+-- -> 1. mark: update records with unique key
+-- -> 2. fetch: select marked records
+-- Note: select on TKE.TaskExtended, but update on TK.Task. may require explicit sql.
+claim :: Constraints -> SqlTransaction Connection [Data]
+claim cs = do
+
+        -- get time
         t <- liftIO $ floor <$> getPOSIXTime
-        update "task" (("time" |<= SqlInteger t) : xs) [] [("claim", toSql i)]
-        search ["claim" |== toSql i] [] 10000 0 
 
--- process a task
-process :: TKE.TaskExtended -> SqlTransaction Connection ()
-process s = do
+        -- cleanup deleted tasks (triggers are deleted cascading)
+        transaction sqlExecute $ Delete (table "task") ["time" |<= SqlInteger (t - 24*60*60), "deleted" |== SqlBool True]
 
-        unset $ fromJust $ TKE.id s 
-        
-        case TKE.type s of
+        -- fetch tasks
+        ss <- search (("time" |<= SqlInteger t) : cs) [] 10000 0 
 
-                "modify_money" -> undefined
+        -- mark tasks deleted and get data
+        forM ss $ \s -> do
+            save $ s { TK.deleted = True }
+            return $ unpack' $ TK.data s
 
-                "modify_experience" ->undefined 
+-- process an action
+process :: Data -> SqlTransaction Connection ()
+process d = do
 
-                "transfer_money" -> undefined 
+         case get "action" d :: Maybe Action of
 
-                "transfer_car" -> undefined 
+                Just "modify_money" -> undefined
 
-                "give_part" -> undefined 
+                Just "modify_experience" ->undefined 
 
-                "track_time" -> do
+                Just "transfer_money" -> undefined 
 
-                        let d :: DataTrackTime = fromJust $ unpack $ TKE.data s
-                        save $ (def :: TTM.TrackTime) { TTM.account_id = dtt_account_id d, TTM.track_id = dtt_track_id d, TTM.time = dtt_time d  }
+                Just "transfer_car" -> undefined 
+
+                Just "give_part" -> undefined 
+
+                Just "track_time" -> do
+                        save $ (def :: TTM.TrackTime) { TTM.account_id = get' "account_id" d, TTM.track_id = get' "track_id" d, TTM.time = get' "time" d  }
                         return ()
 
-                e -> fali $ "unknown task type: " ++ e
+                Just e -> fali $ "process: unknown action: " ++ e
+                Nothing -> fali "process: no action specified"
 
 -- failing tasks should not break transactions
 -- TODO: store error report in db or sth
--- backend/game/src/Model/Task.hs
+++ backend/game/src/Model/Task.hs
@@ -28,7 +28,6 @@ $(genAll "Task" "task" [
                     ("type", ''String),
                     ("time", ''Integer),
                     ("data", ''C.ByteString),
-                    ("deleted", ''Bool),
-                    ("claim", ''Integer)
+                    ("deleted", ''Bool)
    ])
 
