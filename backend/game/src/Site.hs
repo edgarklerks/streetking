c77cfb416479a486c43a6bd3aa5c0eafc0d816af
justin <justin@localhost>
2012-08-05 19:00:15 +0200

    pancho is pnacoh

--- backend/game/src/Site.hs
+++ backend/game/src/Site.hs
@@ -1553,7 +1553,7 @@ raceChallengeAccept = do
 
             -- retrieve challenge
             chg <- aget ["id" |== toSql cid, "account_id" |<> toSql uid, "deleted" |== toSql False] (rollback "challenge not found") :: SqlTransaction Connection Chg.Challenge
-            chge <- aget ["challenge_id" |== toSql chg] (rollback "challenge_ext not found") :: SqlTransaction Connection ChgE.ChallengeExtended
+            chge <- aget ["challenge_id" |== toSql cid] (rollback "challenge_ext not found") :: SqlTransaction Connection ChgE.ChallengeExtended
 
             -- TODO: check user busy
 
@@ -1564,7 +1564,7 @@ raceChallengeAccept = do
             oa <- aget ["id" |== (toSql $ A.id $ Chg.account chg)] (rollback "opponent current account not found") :: SqlTransaction Connection A.Account
             ma <- aget ["id" |== toSql uid] (rollback "account minimal not found") :: SqlTransaction Connection APM.AccountProfileMin
             c <- aget ["account_id" |== toSql uid .&& "active" |== toSql True] (rollback "Active car not found") :: SqlTransaction Connection CIG.CarInGarage
---            oc <- aget ["id" |== (toSql $ CIG.id $ Chg.car chg)] (rollback "opponent challenge car not found") :: SqlTransaction Connection CIG.CarInGarage
+            oc <- aget ["id" |== (toSql $ CIG.id $ Chg.car chg)] (rollback "opponent challenge car not found") :: SqlTransaction Connection CIG.CarInGarage
            
             tr <- aget ["track_id" |== (SqlInteger $ Chg.track_id chg), "track_level" |<= (SqlInteger $ A.level a), "city_id" |== (SqlInteger $ A.city a)] (rollback "track not found") :: SqlTransaction Connection TT.TrackMaster
             ts <- agetlist ["track_id" |== (SqlInteger $ TT.track_id tr) ] [] 1000 0 (rollback "track data not found") :: SqlTransaction Connection [TD.TrackDetails]
@@ -1577,9 +1577,9 @@ raceChallengeAccept = do
             let ors = raceResult2FE $ runRace trk (accountDriver $ Chg.account chg) (carInGarageCar $ Chg.car chg) env
 
             -- decide on winner
- --           let (winner, wcar, loser, lcar) = case (raceTime yrs) < (raceTime ors) of
---                    True -> (a, c, oa, oc)
---                    False -> (oa, oc, a, c)
+            let (winner, wcar, loser, lcar) = case (raceTime yrs) < (raceTime ors) of
+                    True -> (a, c, oa, oc)
+                    False -> (oa, oc, a, c)
 
             -- time 
             t <- liftIO (floor <$> getPOSIXTime :: IO Integer)
@@ -1599,13 +1599,13 @@ raceChallengeAccept = do
             -- task update race times on user finish
             Task.trackTime (t + (ceiling $ raceTime yrs)) (Chg.track_id chg) (fromJust $ A.id a) (raceTime yrs)
             Task.trackTime (t + (ceiling $ raceTime ors)) (Chg.track_id chg) (fromJust $ A.id oa) (raceTime ors)
-{-
+
             -- task transfer challenge object
             case ChgE.type chge of
                 "money" -> Task.transferMoney te (fromJust $ A.id loser) (fromJust $ A.id winner) (Chg.amount chg)
                 "car" -> Task.transferCar te (fromJust $ A.id loser) (fromJust $ A.id winner) (fromJust $ CIG.id lcar)
                 _ -> rollback $ "challenge type not recognized: " ++ (ChgE.type chge)
-  -}          
+      
             -- TODO: other race rewards (give money, respect, part)
 
             -- return race id
