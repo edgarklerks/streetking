a2a66ac003acfb830707af12d87fc3edf0843157
edgar <edgar@localhost>
2012-08-22 12:38:29 +0000

    added tournament etcx

---
+++ backend/game-new/src/Model/TournamentPlayers.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell, OverloadedStrings #-}
+module Model.TournamentPlayers where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+import qualified Data.Aeson as AS
+import           Data.InRules
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as B
+import           Data.Racing 
+
+import qualified Data.HashMap.Strict as HM
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+$(genAll "TournamentPlayer" "tournament_players" 
+    [
+        ("id", ''Id),
+        ("account_id", ''Id),
+        ("tournament_id", ''Id)
+    ])
