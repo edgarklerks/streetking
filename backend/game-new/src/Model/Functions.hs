d78a614e6920029165d18f470aecbcf7f48d9de5
edgar <edgar@localhost>
2012-10-04 12:56:36 +0200

    gwum

---
+++ backend/game-new/src/Model/Functions.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+module Model.Functions where 
+
+import Model.DBFunctions 
+import Data.SqlTransaction
+import Database.HDBC (toSql, fromSql)
+import Data.Database
+import Data.HashMap.Strict
+
+import qualified Model.Task as TK
+
+$(mkFunctions [
+        ("account_update_energy", [''Integer], ''Bool, Scalar),
+        ("personnel_train", [''Integer, ''String, ''String], ''Bool, Scalar),
+        ("personnel_start_task", [''Integer, ''String, ''Integer], ''Bool, Scalar),
+        ("personnel_cancel_task", [''Integer], ''Bool, Scalar),
+        ("garage_actions_account", [''Integer], ''Bool, Scalar),
+        ("garage_car_ready", [''Integer, ''Integer], ''String, Row),
+        ("garage_active_car_ready", [''Integer], ''String, Row),
+        ("garage_set_active_car", [''Integer, ''Integer], ''Bool, Scalar),
+        ("garage_unset_active_car", [''Integer, ''Integer], ''Bool, Scalar),
+        ("claim_tasks", [''Integer, ''Integer, ''Integer], ''TK.Task, Row),
+        ("tasks_in_progress", [''Integer, ''Integer, ''Integer], ''Bool, Scalar),
+        ("unix_timestamp", [], ''Integer, Scalar)
+    ])
