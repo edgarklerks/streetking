02f0e4eb6bfe1af65684f497c8bc698330e96546
edgar <edgar@localhost>
2012-10-04 19:05:35 +0200

    start task replaced

---
+++ backend/game/src/Model/TaskTrigger.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell, OverloadedStrings #-}
+module Model.TaskTrigger where 
+
+import           Data.SqlTransaction
+import           Database.HDBC
+import           Data.Convertible
+import           Model.General
+import           Data.Database 
+import           Control.Monad
+import qualified Data.Aeson as AS
+import Data.InRules
+
+import qualified Data.HashMap.Strict as HM
+import           Control.Applicative
+import qualified Data.Map as M
+import           Model.TH
+import           Prelude hiding (id)
+
+
+$(genAll "TaskTrigger" "task_trigger" [
+                    ("id", ''Id),
+                    ("task_id", ''Integer),
+                    ("type", ''Integer),
+                    ("target_id", ''Integer)
+   ])
+
---
+++ templates/main/sub_header.tpl
@@ -0,0 +1,6 @@
+<div id="sub-header-box">
+	<div id="user-photo-default" class="ui-corner-all-4px"></div>
+	<div id="user-photo" style="background-image:url([:picture_small])" class="ui-corner-all-4px"></div>
+	<div id="user-information" class="user_timer"></div>
+	<div id="user-notification" module="SHOW_NOTIFICATIONS" class="notification_timer">0</div>
+</div>
