2006f9ecc3305a76fadfa44ebe1982d960639bb2
justin <justin@localhost>
2012-10-30 14:34:11 +0100

    a

--- backend/admin/src/Model/CarInstance.hs
+++ backend/admin/src/Model/CarInstance.hs
@@ -22,6 +22,9 @@ $(genAll "CarInstance" "car_instance"
         ("id", ''Id),
         ("car_id", ''Integer),
         ("garage_id", ''MInteger),
-        ("deleted", ''Bool)
+        ("deleted", ''Bool),
+        ("prototype", ''Bool),
+        ("prototype_name", ''String),
+        ("prototype_available", ''Bool)
     ]
     )
