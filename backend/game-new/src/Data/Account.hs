f64a5e85422d0362d458f005fbf66abfe8eac2d5
edgar <edgar@localhost>
2012-11-20 20:12:32 +0100

    diff

--- backend/game-new/src/Data/Account.hs
+++ backend/game-new/src/Data/Account.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, ViewPatterns #-}
 module Data.Account (
-        respectNext,
         addRespect
     ) where
 
@@ -16,40 +15,41 @@ import Data.InRules
 import Data.Conversion
 import Model.General
 import Debug.Trace
+import Control.Monad 
 
 addRespect :: Integer -> Integer -> SqlTransaction Connection ()
 addRespect uid amt = do
         a <- aload uid (rollback $ "account not found for id " ++ (show uid)) :: SqlTransaction Connection A.Account
-        trace "updating account" $ update "account" ["id" |== (toSql $ uid)] [] [("respect", toSql $ (A.respect a) + amt)]
-        trace "checking level" $ checkLevel uid
+        update "account" ["id" |== (toSql $ uid)] [] [("respect", toSql $ (A.respect a) + amt)]
+        checkLevel uid
 
-respectNext :: Integer -> SqlTransaction Connection Integer 
-respectNext uid = do
-        a <- aload uid (rollback $ "account not found for id " ++ (show uid)) :: SqlTransaction Connection A.Account
-        k :: Integer <- CFG.getKey "respect_for_level"
-        let r = (\l -> k * ((l ^ 2) + l) `div` 2) $ A.level a
-        trace ("needed for next level: " ++ (show $ r - (A.respect a))) return $ r - (A.respect a) 
+respFunc :: Integer -> Integer -> Integer 
+respFunc (fromInteger -> k) (fromInteger -> l) = floor $ k * ((l**2) + l) / 2
+
+levelFunc :: Integer -> Integer -> Integer
+levelFunc (fromInteger -> k) (fromInteger -> r) = floor $ (sqrt (k + 8 * r) - sqrt k ) / (2  * sqrt k)
 
 checkLevel :: Integer -> SqlTransaction Connection ()
 checkLevel uid = do
-        r <- respectNext uid
-        case r < 0 of
-                False -> trace "no update needed" $ return ()
-                True -> do
-                        trace "leveling up" $ levelUp uid
-                        checkLevel uid
-
-levelUp :: Integer -> SqlTransaction Connection () 
-levelUp uid = do
-        s :: Integer <- CFG.getKey "skillpoints_per_level"
-        e :: Integer <- CFG.getKey "energy_per_level"
+        k :: Integer <- CFG.getKey "respect_for_level"
         a <- aload uid (rollback $ "account not found for id " ++ (show uid)) :: SqlTransaction Connection A.Account
-        let fl = A.level a
-        let su = A.skill_unused a
-        let en = (A.max_energy a) + e
-        update "account" ["id" |== (toSql $ uid)] [] [("level", toSql $ fl + 1), ("skill_unused", toSql $ su + s), ("max_energy", toSql en), ("energy", toSql en)]
-        -- TODO: level-up notification 
-        return ()
-         
+        let nl = levelFunc k (A.respect a)
+        when (nl > A.level a ) $ do 
+                    s :: Integer <- CFG.getKey "skillpoints_per_level"
+                    e :: Integer <- CFG.getKey "energy_per_level"
+                    let en = (A.max_energy a) + e * (nl - A.level a)
+                    let su = A.skill_unused a
+                    void $ update "account" 
+                                    [
+                                        "id" |== (toSql $ uid)
+                                    ] [] 
+                                    [  
+                                        ("level", toSql $ nl), 
+                                        ("skill_unused", toSql $ su + s), 
+                                        ("max_energy", toSql en),
+                                        ("energy", toSql en)
+                                    ]
+
+
 
      
