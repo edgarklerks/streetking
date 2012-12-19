8292d54834fed4a229f2d273d43c6a2d8ac35224
siplord <siplord@localhost>
2012-12-19 11:06:36 +0100

    templates autocommit

--- backend/proxy-new/src/Site.hs
+++ backend/proxy-new/src/Site.hs
@@ -145,11 +145,15 @@ transparent = do
 
 identify = do 
         b <- getOpParam "token"
+        liftIO $ print b
         u <- runDb $ (search ["token" |== toSql b] [] 1 0 :: SqlTransaction Connection [A.Application])
+        liftIO $ print u
         withTop roles $ 
             case u of 
                 [] -> internalError "need token"
-                [u] -> addRole "application_token" (Developer $ A.id u)  >> return ()
+                [u] -> do
+                    liftIO $ print u 
+                    addRole "application_token" (Developer $ A.id u)  >> return ()
 
 runDb x = with sql $ S.runDb internalError  x
 
