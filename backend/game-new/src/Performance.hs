816c08c8e40e47cd51b766b1d9b4793830f44e0e
edgar <edgar@localhost>
2013-01-03 13:30:44 +0100

    get views untangled

--- backend/game-new/src/Performance.hs
+++ backend/game-new/src/Performance.hs
@@ -16,7 +16,7 @@ import qualified Model.Account as A
 import Data.Maybe 
 
 
-main = do
+mainQA = do
     xs <- fmap (fromJust . A.id) <$> getAccountIds 
     defaultMain [
         bench "sql query account " (whnfIO $ sqlQueryAccount xs)
@@ -29,7 +29,21 @@ sqlQueryAccount :: [Integer] -> IO ()
 sqlQueryAccount xs = runTestDb $ do 
                 forM_ xs $ \i -> do 
                         load i :: SqlTransaction Connection (Maybe A.Account)
-            
+
+main = defaultMain [
+        bench "cache trigger model" $ runTestDb $ replicateM_ 100 $ do 
+                quickQuery "update garage_parts set part_instance_id = part_instance_id where part_instance_id = 3147" []   
+                quickQuery "update car_3d_model set part_instance_id = part_instance_id where part_instance_id = 3147" []
+                quickQuery "update market_place set id = id where id = 3147" [],
+        bench "cache trigger model parallel" $ runTestDb $ replicateM_ 100 $ do 
+                forkSqlTransaction $ quickQuery "update garage_parts set part_instance_id = part_instance_id where part_instance_id = 3147" []   
+                forkSqlTransaction $ quickQuery "update car_3d_model set part_instance_id = part_instance_id where part_instance_id = 3147" []
+                forkSqlTransaction $ quickQuery "update market_place set id = id where id = 3147" []
+
+    ]
+
+
+
 mainP2P = do 
     qc <- init_query 'a'
     let ps = take 10000 $ ((B.pack . show <$> [0..]) `zip` (B.pack . show <$> [0..]))
@@ -52,6 +66,6 @@ search_query qc ps = forM_ ps $ \(key, v) -> do
 
 init_query c = do 
         ms <- newMemState (1000 * 1000 * 1000 * 60 * 60) (6000 * 1000 * 1000) (c : "testshitas")
-        qc  <- newTChanIO
+        qc  <- newTQueueIO
         forkIO $ queryManager (c : "testshitas") ms qc
         return qc 
