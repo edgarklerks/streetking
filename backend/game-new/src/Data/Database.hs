6ca55e5dc724ddd037ea0061d1d374dffe0254a4
eklerks <eklerks@localhost>
2013-01-21 11:17:01 +0100

    changed database

--- backend/game-new/src/Data/Database.hs
+++ backend/game-new/src/Data/Database.hs
@@ -22,7 +22,7 @@ import Database.HDBC.PostgreSQL
  -}
 
 dbconn ::  IO Connection
-dbconn = connectPostgreSQL "host=r3.graffity.me port=5432 dbname=streetking_dev user=deosx password=#*rl&" 
+dbconn = connectPostgreSQL "host=localhost port=5432 dbname=postgres user=postgres password=wetwetwet"
 
 -- dbconn = connectPostgreSQL "host=192.168.1.77 port=5439 dbname=deosx user=postgres password=wetwetwet" 
 doSql :: SqlTransaction Connection a -> IO a
--- backend/proxy-new/src/Data/Database.hs
+++ backend/proxy-new/src/Data/Database.hs
@@ -22,7 +22,7 @@ import Database.HDBC.PostgreSQL
  -}
 
 dbconn ::  IO Connection
-dbconn = connectPostgreSQL "host=172.20.0.250 port=5432 dbname=streetking_dev user=deosx password=#*rl&"
+dbconn = connectPostgreSQL "host=localhost port=5432 dbname=postgres user=postgres password=wetwetwet"
 
 doSql :: SqlTransaction Connection a -> IO a
 doSql t = dbconn >>= (runSqlTransaction t error)
