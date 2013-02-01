938438e4e43048bbf174cf92f08491260267dd20
eklerks <eklerks@localhost>
2013-02-01 15:39:18 +0100

    i forgets to add moar files

---
+++ backend/proxy-new/src/Data/ExternalLog.hs
@@ -0,0 +1,35 @@
+module Data.ExternalLog where 
+
+import System.ZMQ3
+import Control.Concurrent.STM 
+import Control.Concurrent 
+import Control.Monad
+import Data.Monoid 
+import Data.ByteString.Char8 (pack)
+
+
+type Address = String 
+type Name = String 
+data Cycle = Cycle {
+        cycleChannel :: TQueue (String, String),
+        threadId :: ThreadId 
+    }
+
+reportCycle :: Cycle -> String -> String -> IO ()
+reportCycle a group component = atomically $ writeTQueue (cycleChannel a) (group, component) 
+
+reportCycleSTM :: Cycle -> String -> String -> STM ()
+reportCycleSTM a group component = writeTQueue (cycleChannel a) (group, component) 
+
+
+
+initCycle :: Address -> IO Cycle 
+initCycle a = do 
+        q <- newTQueueIO 
+        tid <- forkIO $ withContext $ \c -> withSocket c Pub $ \s -> do 
+                bind s a  
+                forever $ do 
+                    (g, p) <- atomically $ readTQueue q
+                    send s [] (pack $ g <> " " <> p)
+        return $ Cycle q tid 
+
