63488ef2a7682f674dbdd37f70aa54f1aa43b667
edgar <edgar@localhost>
2013-07-03 05:11:40 -0700

    added documentation

--- backend/game-new/src/Application.hs
+++ backend/game-new/src/Application.hs
@@ -94,25 +94,31 @@ type Application = AppHandler
 toAeson :: InRule -> L.ByteString  
 toAeson = (Data.Aeson.encode :: Value -> L.ByteString) . fromInRule
 
+-- | Write Aeson to user 
 writeAeson :: ToInRule a => a -> Application ()
 writeAeson = writeLBS . toAeson . toInRule
 
+-- | Write an error to the http client
 writeError :: ToInRule a => a -> Application ()
 writeError x = writeAeson $ S.fromList [("error" :: String, x)]
 
+-- | Write a InRule to the http client  
 writeResult :: ToInRule a => a -> Application ()
 writeResult x = writeAeson $ S.fromList [("result" :: String, x)]
 
+-- | Write a JSON to the http client  
 writeResult' :: ToJSON a => a -> Application ()
 writeResult' x = writeAeson' $ S.fromList [("result" :: String, x)]
 
-
+-- | Write a mapable to the client 
 writeMapable :: Mapable a => a -> Application ()
 writeMapable = writeResult . toHashMap 
 
+-- | Write multiple mapables as a json array 
 writeMapables :: Mapable a => [a] -> Application ()
 writeMapables = writeResult . fmap toHashMap 
 
+-- | get the user id from the proxy 
 getUserId :: Application Integer 
 getUserId =  do 
         x <- getParam "userid"
@@ -120,7 +126,7 @@ getUserId =  do
                 Just y -> return (read $ B.unpack y)
                 Nothing -> internalError "No userid"
 
-
+-- | get a GET param faults when param doesn't exist 
 getOParam :: B.ByteString -> Application B.ByteString
 getOParam x = do 
     p <- getParam x
@@ -129,21 +135,24 @@ getOParam x = do
         Just a -> return a
 
 type SqlMap = S.HashMap String SqlValue 
+-- | get json in the form of a 'SqlMap'  
 getJson :: Application  SqlMap 
 getJson = do 
     t <- parse json <$> getRequestBody 
     case t of 
         (Done _ v)  -> return (fromInRule $ toInRule v)
         otherwise -> return S.empty --- internalError "No json provided" 
-
+-- | Get multiple jsons as a 'SqlMap' 
 getJsons :: Application [SqlMap]
 getJsons = do 
     t <- parse json <$> getRequestBody 
     case t of 
         (Done _ v)  -> return (fromInRule $ toInRule v)
         otherwise -> return [] --- internalError "No json provided" 
-
-getPagesWithDTDOrdered :: [String] -> DTD -> Application (((Integer, Integer), Constraints), Orders)
+-- | Generates a constraint and returns an (((limit, offset), Constraints),Orders)
+getPagesWithDTDOrdered :: [String] -- ^ Allowed order fields 
+            -> DTD  -- ^ search expression
+            -> Application (((Integer, Integer), Constraints), Orders)
 getPagesWithDTDOrdered vs d = do 
                 xs <- getJson
                 let b = S.lookupDefault (DB.SqlInteger 100) "limit" xs
@@ -155,8 +164,15 @@ getPagesWithDTDOrdered vs d = do
                     Left e -> internalError e
                     Right x -> do 
                         return (((DB.fromSql b, DB.fromSql o), cst), x)
-
-getPagesWithDTDOrderedAndParams :: SqlMap -> [String] -> DTD -> Application (((Integer, Integer), Constraints), Orders)
+-- | Same as 'getPagsWithDTDOrdered', only then provide default values  
+getPagesWithDTDOrderedAndParams :: 
+        SqlMap -- ^ Default values 
+        -> 
+        [String] -- ^ Allowed ordered fields 
+        -> 
+        DTD -- ^ search expression, see 'Data.DatabaseTemplate' 
+        -> 
+        Application (((Integer, Integer), Constraints), Orders)
 getPagesWithDTDOrderedAndParams xs vs d = do 
                 let b = S.lookupDefault (DB.SqlInteger 100) "limit" xs
                 let o = S.lookupDefault (DB.SqlInteger 0) "offset" xs 
@@ -170,6 +186,7 @@ getPagesWithDTDOrderedAndParams xs vs d = do
 
 
 
+-- | Generate a constraint and a limit and offset 
 getPagesWithDTD :: DTD -> Application ((Integer, Integer), Constraints)
 getPagesWithDTD d = do 
         xs <- getJson 
@@ -179,43 +196,46 @@ getPagesWithDTD d = do
 
 
 
+-- | Add a role to a user id 
 addRole i k =  with nde $ do 
                     sendQuery (ProtoExtended.insert k rs) 
         where rs = B.pack $ L.unpack $ BI.encode $ User (Just i) 
 
 
-
+-- | write an json error 
 writeError' :: ToJSON a => a -> Application ()
 writeError' x = writeAeson' $ S.fromList [("error" :: String, x)]
 
 
+-- | Write an error to the client
 writeAeson' :: ToJSON a => a -> Application ()
 writeAeson' = writeLBS . Data.Aeson.encode
 
 
 
-
+-- | Throws an internal error code 500
 internalError :: String -> Application a 
 internalError x = modifyResponse (setResponseCode 500) *> (CIO.throw $ UserErrorE (B.pack x))
 
 
-
+-- | Run a composemonad in the SqlTransaction monad 
 runCompose m = do
             l <- getLock 
             with db $ withConnection $ \c -> do 
                 frp <- runComposeMonad l m error c
                 frp `seq` return frp 
 
-
+-- | Do an SqlTransaction action with a non blocking lock 
 withLockBlock n a m =  do 
             l <- getLock 
             with slock $ do
                 L.withLockBlock l n a m 
 
+-- | Do a SqlTransaction with a blocking lock 
 withLockNonBlock n a m = do  
             l <- getLock 
             with slock $ do
                 L.withLockNonBlock l n a m  
-
+-- | Retrieve the lock manager
 getLock :: Application L.Lock 
 getLock = with slock $ L.getLock  
