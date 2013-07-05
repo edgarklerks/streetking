37d77bd42809876aed151d9a85fe721be6b6d193
edgar <edgar@localhost>
2013-07-05 12:10:31 -0700

    removed old proxy

--- backend/proxy/src/DHA.hs
+++
@@ -1,9 +0,0 @@
-
-import Text.Parsec
-import Text.Parsec.Token
-import Text.Parsec.Combinator
-import Text.Parsec.Prim
-
-
-test :: Parsec Integer () Bool 
-test = undefined
--- backend/proxy/src/Snap/Extension/bla.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
---infixl 2 `band` 
---infixl 1 `bor` 
-bor a b = a || b
-band a b = a && b
